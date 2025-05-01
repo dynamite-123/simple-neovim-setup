1e93c42576cbe69c1f6384c78938924e8be4dc71
dynamite-123 <anirudhkashyap182005@gmail.com>
2025-05-01 03:17:28 +0000

    auto relead

--- lua/josean/core/options.lua
+++ lua/josean/core/options.lua
@@ -34,3 +34,30 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split windows
 opt.splitright = true
 opt.splitbelow = true
+
+-- auto refresh files
+opt.autoread = true                -- auto-reload files changed outside of Vim
+opt.updatetime = 300               -- faster completion (default is 4000ms)
+
+-- Create compact autocommands for file change detection
+local autoread_group = vim.api.nvim_create_augroup("AutoRead", { clear = true })
+
+-- Combined handler for all relevant events
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    group = autoread_group,
+    pattern = "*",
+    callback = function()
+        if vim.api.nvim_get_mode().mode ~= "c" then  -- Don't refresh when in command line mode
+            vim.cmd("checktime")
+        end
+    end,
+})
+
+-- Notification after file change
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = autoread_group,
+    pattern = "*",
+    callback = function()
+        vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN)
+    end,
+})
