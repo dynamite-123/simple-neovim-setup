eb6515e25dc4e1f1fe4deb6d4da059f51f1942f8
Anirudh Kashyap <anirudhkashyap182005@gmail.com>
2025-08-19 16:34:33 +0000

    fixed bugs

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -28,4 +28,18 @@ keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true, desc = "Unind
 
 -- indent / unindent in normal mode
 keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true, desc = "Indent line" })
+
+keymap.set("n", "<leader>rr", function()
+		local filename = vim.fn.expand("%")
+		if vim.bo.filetype == "cpp" then
+			-- Split horizontally for terminal at the bottom
+			vim.cmd("botright split | resize 15 | term g++ " .. filename .. " -o a.out && ./a.out < input.txt")
+			-- Move to the terminal window
+			vim.cmd("wincmd j")
+			-- Open input.txt in a vertical split to the right of the terminal (bottom right)
+			vim.cmd("vsplit input.txt")
+		else
+			print("Not a C++ file.")
+		end
+	end, { desc = "Compile/run C++ with input.txt and open input.txt on right" })
 keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true, desc = "Unindent line" })
